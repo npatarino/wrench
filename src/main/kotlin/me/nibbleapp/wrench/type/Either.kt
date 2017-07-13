a0d71c0e9696cf9d315ca4ac771a03315e34b201
Nicolás Patarino <npatarino@idealista.com>
2017-07-13 10:36:34 +0200

    Extends Either object

--- src/main/kotlin/me/nibbleapp/wrench/type/Either.kt
+++ src/main/kotlin/me/nibbleapp/wrench/type/Either.kt
@@ -2,16 +2,54 @@ package me.nibbleapp.wrench.type
 
 sealed class Either<out L, out R> {
 
-  data class Left<out T>(val value: T) : Either<T, Nothing>()
-  data class Right<out T>(val value: T) : Either<Nothing, T>()
+    data class Left<out T>(val value: T) : Either<T, Nothing>()
+    data class Right<out T>(val value: T) : Either<Nothing, T>()
 
-  fun fold(left: (L) -> Unit, right: (R) -> Unit): Unit = when (this) {
-    is Left  -> left(value)
-    is Right -> right(value)
-  }
+    /**
+     * Example:
+     * ```
+     * val folded: Either<Error, Value> = someMethod()
+     * result.fold(
+     *      { log("Error with $it") },
+     *      { log("Success with $it") }
+     * )
+     * ```
+     */
+    inline fun <C> fold(left: (L) -> C, right: (R) -> C): C = when (this) {
+        is Left -> left(value)
+        is Right -> right(value)
+    }
 
-  fun swap() = fold({ Right(it) }, { Left(it) })
+    /**
+     * Example:
+     * ```
+     * Left(error).swap()   // Result: Right("Maradona")
+     * Right("Maradona").swap() // Result: Left(error)
+     * ```
+     */
+    fun swap() = fold({ Right(it) }, { Left(it) })
 
+    /**
+     * Example:
+     * ```
+     * Right("Maradona").map { "Diego" } // Result: Right("Diego")
+     * Left(error).map { "Diego" }  // Result: Left(error)
+     * ```
+     */
+    inline fun <C> map(f: (R) -> C): Either<L, C> =
+            fold({ Left(it) }, { Right(f(it)) })
+
+    /**
+     * Returns the value from this [Either.Right] or the given argument if this is a [Either.Left].
+     *
+     * Example:
+     * ```
+     * Right("Maradona").getOrElse("undefined") // Result: "Maradona"
+     * Left(error).getOrElse("undefined")  // Result: "undefined"
+     * ```
+     */
+    inline fun <B> Either<*, B>.getOrElse(default: () -> B): B =
+            fold({ default() }, { it })
 }
 
 
