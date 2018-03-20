7f586e5b0a9a41e00f6f61efae739c53b4412290
Jose del Pozo <josedlpozo@gmail.com>
2018-03-20 20:40:33 +0100

    Move ApplicativeOption to a static way Add example

--- src/main/kotlin/com/idealista/android/wrench/type/ApplicativeOption.kt
+++ src/main/kotlin/com/idealista/android/wrench/type/ApplicativeOption.kt
@@ -2,42 +2,44 @@ package com.idealista.android.wrench.type
 
 class ApplicativeOption {
 
-    fun <A, B> product(first: Option<A>, second: Option<B>): Option<Tuple2<A, B>> = first.flatMap { first ->
-        second.map {
-            Tuple2(first, it)
+    companion object {
+        fun <A, B> product(first: Option<A>, second: Option<B>): Option<Tuple2<A, B>> = first.flatMap { first ->
+            second.map {
+                Tuple2(first, it)
+            }
         }
+
+        fun <A, B, C> product(first: Option<A>, second: Option<B>, third: Option<C>): Option<Tuple3<A, B, C>> =
+                product(first, second).flatMap { product ->
+                    third.map {
+                        Tuple3(product.first, product.second, it)
+                    }
+                }
+
+        fun <A, B, C, D> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>): Option<Tuple4<A, B, C, D>> =
+                product(first, second, third).flatMap { product ->
+                    fourth.map {
+                        Tuple4(product.first, product.second, product.third, it)
+                    }
+                }
+
+        fun <A, B, C, D, E> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>): Option<Tuple5<A, B, C, D, E>> =
+                product(first, second, third, fourth).flatMap { product ->
+                    fifth.map {
+                        Tuple5(product.first, product.second, product.third, product.fourth, it)
+                    }
+                }
+
+        fun <A, B, R> map(first: Option<A>, second: Option<B>, some: (Tuple2<A, B>) -> R): Option<R> =
+                product(first, second).map(some)
+
+        fun <A, B, C, R> map(first: Option<A>, second: Option<B>, third: Option<C>, some: (Tuple3<A, B, C>) -> R): Option<R> =
+                product(first, second, third).map(some)
+
+        fun <A, B, C, D, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, some: (Tuple4<A, B, C, D>) -> R): Option<R> =
+                product(first, second, third, fourth).map(some)
+
+        fun <A, B, C, D, E, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, some: (Tuple5<A, B, C, D, E>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth).map(some)
     }
-
-    fun <A, B, C> product(first: Option<A>, second: Option<B>, third: Option<C>): Option<Tuple3<A, B, C>> =
-            product(first, second).flatMap { product ->
-                third.map {
-                    Tuple3(product.first, product.second, it)
-                }
-            }
-
-    fun <A, B, C, D> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>): Option<Tuple4<A, B, C, D>> =
-            product(first, second, third).flatMap { product ->
-                fourth.map {
-                    Tuple4(product.first, product.second, product.third, it)
-                }
-            }
-
-    fun <A, B, C, D, E> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>): Option<Tuple5<A, B, C, D, E>> =
-            product(first, second, third, fourth).flatMap { product ->
-                fifth.map {
-                    Tuple5(product.first, product.second, product.third, product.fourth, it)
-                }
-            }
-
-    fun <A, B, R> map(first: Option<A>, second: Option<B>, some: (Tuple2<A, B>) -> R): Option<R> =
-            product(first, second).map(some)
-
-    fun <A, B, C, R> map(first: Option<A>, second: Option<B>, third: Option<C>, some: (Tuple3<A, B, C>) -> R): Option<R> =
-            product(first, second, third).map(some)
-
-    fun <A, B, C, D, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, some: (Tuple4<A, B, C, D>) -> R): Option<R> =
-            product(first, second, third, fourth).map(some)
-
-    fun <A, B, C, D, E, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, some: (Tuple5<A, B, C, D, E>) -> R): Option<R> =
-            product(first, second, third, fourth, fifth).map(some)
 }
--- src/test/kotlin/com/idealista/android/wrench/type/ApplicativeOptionTest.kt
+++ src/test/kotlin/com/idealista/android/wrench/type/ApplicativeOptionTest.kt
@@ -8,7 +8,7 @@ class ApplicativeOptionTest {
 
     @Test fun `given two somes, applicative should call the lambda and return a some`() {
         val option1 : Option<Int> = Option.Some(1)
-        val sum = ApplicativeOption().map(option1, Option.Some(2)) { (first, second) ->
+        val sum = ApplicativeOption.map(option1, Option.Some(2)) { (first, second) ->
             first + second
         }
 
@@ -17,7 +17,7 @@ class ApplicativeOptionTest {
 
     @Test fun `given one some and none, applicative should not call the lambda and return a none`() {
         val option1 : Option<Int> = Option.None
-        val sum = ApplicativeOption().map(option1, Option.Some(2)) { (first, second) ->
+        val sum = ApplicativeOption.map(option1, Option.Some(2)) { (first, second) ->
             first + second
         }
 
@@ -26,7 +26,7 @@ class ApplicativeOptionTest {
 
     @Test fun `given three somes, applicative should call the lambda and return a some`() {
         val option1 : Option<Int> = Option.Some(1)
-        val sum = ApplicativeOption().map(option1, Option.Some(2), Option.Some(2)) { (first, second, three) ->
+        val sum = ApplicativeOption.map(option1, Option.Some(2), Option.Some(2)) { (first, second, three) ->
             first + second + three
         }
 
@@ -35,7 +35,7 @@ class ApplicativeOptionTest {
 
     @Test fun `given two somes and none, applicative should not call the lambda and return a none`() {
         val option1 : Option<Int> = Option.None
-        val sum = ApplicativeOption().map(option1, Option.Some(2), Option.Some(2)) { (first, second) ->
+        val sum = ApplicativeOption.map(option1, Option.Some(2), Option.Some(2)) { (first, second) ->
             first + second
         }
 
