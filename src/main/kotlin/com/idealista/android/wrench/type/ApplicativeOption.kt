28a591ed06721a9c7256a5cabba6c57e629ffdae
Jose del Pozo <jmdelpozo@idealista.com>
2018-04-04 16:10:09 +0200

    Add product, tuples and map method until 10 params

--- src/main/kotlin/com/idealista/android/wrench/type/ApplicativeOption.kt
+++ src/main/kotlin/com/idealista/android/wrench/type/ApplicativeOption.kt
@@ -30,6 +30,41 @@ class ApplicativeOption {
                     }
                 }
 
+        fun <A, B, C, D, E, F> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>): Option<Tuple6<A, B, C, D, E, F>> =
+                product(first, second, third, fourth, fifth).flatMap { product ->
+                    sixth.map {
+                        Tuple6(product.first, product.second, product.third, product.fourth, product.fifth, it)
+                    }
+                }
+
+        fun <A, B, C, D, E, F, G> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>): Option<Tuple7<A, B, C, D, E, F, G>> =
+                product(first, second, third, fourth, fifth, sixth).flatMap { product ->
+                    seventh.map {
+                        Tuple7(product.first, product.second, product.third, product.fourth, product.fifth, product.sixth, it)
+                    }
+                }
+
+        fun <A, B, C, D, E, F, G, H> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>): Option<Tuple8<A, B, C, D, E, F, G, H>> =
+                product(first, second, third, fourth, fifth, sixth, seventh).flatMap { product ->
+                    eighth.map {
+                        Tuple8(product.first, product.second, product.third, product.fourth, product.fifth, product.sixth, product.seventh, it)
+                    }
+                }
+
+        fun <A, B, C, D, E, F, G, H, I> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>, nineth: Option<I>): Option<Tuple9<A, B, C, D, E, F, G, H, I>> =
+                product(first, second, third, fourth, fifth, sixth, seventh, eighth).flatMap { product ->
+                    nineth.map {
+                        Tuple9(product.first, product.second, product.third, product.fourth, product.fifth, product.sixth, product.seventh, product.eighth, it)
+                    }
+                }
+
+        fun <A, B, C, D, E, F, G, H, I, J> product(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>, nineth: Option<I>, tenth: Option<J>): Option<Tuple10<A, B, C, D, E, F, G, H, I, J>> =
+                product(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth).flatMap { product ->
+                    tenth.map {
+                        Tuple10(product.first, product.second, product.third, product.fourth, product.fifth, product.sixth, product.seventh, product.eighth, product.nineth, it)
+                    }
+                }
+
         fun <A, B, R> map(first: Option<A>, second: Option<B>, some: (Tuple2<A, B>) -> R): Option<R> =
                 product(first, second).map(some)
 
@@ -41,5 +76,20 @@ class ApplicativeOption {
 
         fun <A, B, C, D, E, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, some: (Tuple5<A, B, C, D, E>) -> R): Option<R> =
                 product(first, second, third, fourth, fifth).map(some)
+
+        fun <A, B, C, D, E, F, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, some: (Tuple6<A, B, C, D, E, F>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth, sixth).map(some)
+
+        fun <A, B, C, D, E, F, G, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, some: (Tuple7<A, B, C, D, E, F, G>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth, sixth, seventh).map(some)
+
+        fun <A, B, C, D, E, F, G, H, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>, some: (Tuple8<A, B, C, D, E, F, G, H>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth, sixth, seventh, eighth).map(some)
+
+        fun <A, B, C, D, E, F, G, H, I, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>, nineth: Option<I>, some: (Tuple9<A, B, C, D, E, F, G, H, I>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth).map(some)
+
+        fun <A, B, C, D, E, F, G, H, I, J, R> map(first: Option<A>, second: Option<B>, third: Option<C>, fourth: Option<D>, fifth: Option<E>, sixth: Option<F>, seventh: Option<G>, eighth: Option<H>, nineth: Option<I>, tenth: Option<J>, some: (Tuple10<A, B, C, D, E, F, G, H, I, J>) -> R): Option<R> =
+                product(first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, tenth).map(some)
     }
 }
